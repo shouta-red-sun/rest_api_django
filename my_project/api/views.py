b44f186aecdc8e479507da804387d4581337f46b
shouta.red.sun@gmail.com <shouta.red.sun@gmail.com>
2019-09-25 08:51:25 +0000

    views all commit

--- my_project/api/views.py
+++ my_project/api/views.py
@@ -6,9 +6,9 @@ from .serializers import BookSerializer
 
 class BookAPIView(generics.ListAPIView):
 
-    #queryset = Book.objects.all() # all
+    queryset = Book.objects.all() # all
     #queryset = Book.objects.all().order_by('id') # order by
     #queryset = Book.objects.all().order_by('id')[0:10] # order by , 0-10 rec
     #queryset = Book.objects.filter(id__iexact=2) # filter
-    queryset = Book.objects.all().order_by('id')[0:10]
+    #queryset = Book.objects.all().order_by('id')[0:10]
     serializer_class = BookSerializer
